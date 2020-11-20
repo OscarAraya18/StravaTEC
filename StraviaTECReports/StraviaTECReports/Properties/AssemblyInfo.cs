5fa557f88c108b0742eff6cbdfcb3fa3e2eec0e5
Tomás <tmalavassi.r@gmail.com>
2020-11-19 21:45:22 -0600

    Reportes funcionales
    Se añadieron los reportes y su respectiva API

---
+++ StraviaTECReports/StraviaTECReports/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StraviaTECReports")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StraviaTECReports")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("639bb870-0d96-4e23-964f-f695acf64074")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
